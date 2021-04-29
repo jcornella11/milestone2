fb61cbbb3da72d321dca5939851b9d641370ae13
Johnathan Cornella <jcornella11@hotmail.com>
2021-04-28 20:27:28 -0700

    Add project files.

---
+++ milestone1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace milestone1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Driver Code
+
+            Inventory item = new Inventory();
+
+            //Setter Methods for Class
+            string[] searchtags = { "Lightweight", "Steet Running" };
+            item.itemName = "Nike FreeRuns";
+            item.itemPrice = 99.99;
+            item.itemSize = "Medium";
+            item.customerComment = "These are great shoes";
+            item.productCategory = "Running Shoes";
+            item.searchTags = searchtags;
+
+            //Get Methods for Class
+            string itemName = item.itemName;
+            double itemPrice = item.itemPrice;
+            string itemSize = item.itemSize;
+            string customerComment = item.customerComment;
+            string productCategory = item.productCategory;
+            string[] searchresults = item.searchTags;
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ milestone1/Inventory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace milestone1
+{
+    class Inventory
+    {
+        //Atribute Fields
+        private string _itemName;
+        private double _itemPrice;
+        private string _itemSize;
+        private string _customerComment;
+        private string _productCategory;
+        private string[] _searchTags;
+
+        //Constructor Method
+        public Inventory()
+        {
+
+        }
+
+        public string itemName
+        {
+            get { return _itemName; }
+            set { _itemName = value; }
+        }
+
+        public double itemPrice
+        {
+            get { return _itemPrice; }
+            set { _itemPrice = value; }
+        }
+
+        public string itemSize
+        {
+            get { return _itemSize; }
+            set { _itemSize = value; }
+        }
+
+        public string customerComment
+        {
+            get { return _customerComment; }
+            set { _customerComment = value; }
+        }
+
+        public string productCategory
+        {
+            get { return _productCategory; }
+            set { _productCategory = value; }
+        }
+
+        public string[] searchTags 
+        {
+            get { return _searchTags; }
+            set { _searchTags = value; }
+        }
+
+    }
+}
